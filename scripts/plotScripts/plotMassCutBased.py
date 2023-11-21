e0311438630099f4b2e5ce457fdccc43f08d2767
Giovanni Celotto <giovanni.celottog@gmail.com>
2023-11-21 11:29:22 +0100

    add function that saves cut data

--- scripts/plotScripts/plotMassCutBased.py
+++ scripts/plotScripts/plotMassCutBased.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #from getFeaturesBScoreBased import getFeaturesBScoreBased
-from utilsForPlot import loadData, getFeaturesBScoreBased
+from utilsForPlot import loadData, getFeaturesBScoreBased, getXSectionBR
 from fuzzywuzzy import process
 from plotFeaturesBscoreBased4 import plotNormalizedFeatures
 
@@ -44,10 +44,10 @@ def cutSignalAndBackground(signal, realData, featureID, min, max, featureNames =
     return signal, realData
 def main():
     # Loading files
-    signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/withoutArea"
-    realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData"
+    signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/withMoreFeatures"
+    realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData/withMoreFeatures"
 
-    signal, realData = loadData(signalPath=signalPath, realDataPath=realDataPath, nSignalFiles=-1, nRealDataFiles=150)
+    signal, realData = loadData(signalPath=signalPath, realDataPath=realDataPath, nSignalFiles=-1, nRealDataFiles=-1)
 
     # Correction factors and counters
     #N_mini_BPH = np.load("/t3home/gcelotto/ggHbb/outputs/N_mini.npy")
@@ -58,21 +58,41 @@ def main():
     print("Initial Length of signal:\n%d\nInitial Length of bkg:\n%d\n\n"%(len(signal), len(realData)))
 
     print("SIGNAL\n")
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Pt", 22, None, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Eta", -5+2.46, 5-2.46, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Mass", 6, None, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet1_btagDeepFlavB", 0.2, None, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet2Pt", 20, None, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet2Eta", -2, 2, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "twist_Dijet", 0.76, None, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Pt", 45, None, labels) # repeat for process understanding
-    signal, realData = cutSignalAndBackground(signal, realData, "dEta_Dijet", 0, 1.4, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "Jet2_btagDeepFlavB", 0.35, 1, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "DijetMass", 60, 200, labels)
-    signal, realData = cutSignalAndBackground(signal, realData, "ht", 180, None, labels)
-    
-    plotNormalizedFeatures(signal=signal, realData=realData, outFile = "/t3home/gcelotto/ggHbb/outputs/plots/normalizedFeatures_cuts.pdf")
+    mask=(signal[:,8]>0)
+    print(np.min(signal[mask,0]))
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Pt", 20, None, labels)                 # 0
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Mass", 6, None, labels)                # 3
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet1_btagDeepFlavB", 0.2, None, labels)    # 6
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet2Pt", 20, None, labels)                 # 9
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet2Eta", -2, 2, labels)                   # 10
+    signal, realData = cutSignalAndBackground(signal, realData, "twist_Dijet", 0.76, None, labels)          # 26
+    signal, realData = cutSignalAndBackground(signal, realData, "dEta_Dijet", 0, 1.4, labels)               # 23
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet2_btagDeepFlavB", 0.35, 1, labels)      # 15
+    signal, realData = cutSignalAndBackground(signal, realData, "dPhi_Dijet", 1.4, None, labels)            #  24
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet1_QGl", 0.25, None, labels)             # 8
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet2_QGl", 0.25, None, labels)             # 17
+    signal, realData = cutSignalAndBackground(signal, realData, "ht", 180, None, labels)                    # 27
+    signal, realData = cutSignalAndBackground(signal, realData, "Jet1Pt", 45, 180, labels)                  # repeat for process understanding
+    #signal, realData = cutSignalAndBackground(signal, realData, "Jet1Eta", -5+2.46, 5-2.46, labels)
+    #signal, realData = cutSignalAndBackground(signal, realData, "Jet2Pt", 20, None, labels) 
+    #signal, realData = cutSignalAndBackground(signal, realData, "DijetMass", 60, 200, labels)
+    # Save Data after cuts
+    try:
+        np.save("/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/afterCutWithMoreFeatures/signalCut.npy", signal)
+        np.save("/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData/afterCutWithMoreFeatures/realDataCut.npy", realData)
+    except:
+        pass
+        np.save("/t3home/gcelotto/ggHbb/outputs/signalCut.npy", signal)
+        np.save("/t3home/gcelotto/ggHbb/outputs/realDataCut.npy", realData)
+    toKeep = [ 0, 3, 6,
+                9, 10, 15,
+                23, 24, 26,
+                8, 17, 27]
+    plotNormalizedFeatures(signal=signal, realData=realData, outFile = "/t3home/gcelotto/ggHbb/outputs/plots/normalizedFeatures_cuts.png", toKeep=toKeep)
     print("\n\n\n")
+    
+
+    #Try new cuts
     #for i in np.linspace(0, 400, 200):
     #    Newsignal, NewrealData = cutSignalAndBackground(signal, realData, "ht", i, None, labels)
     
