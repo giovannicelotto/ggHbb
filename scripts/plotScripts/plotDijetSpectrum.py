99f9ac268f07f0ea9ad193c7665e2a5a8da278e5
Giovanni Celotto <giovanni.celottog@gmail.com>
2023-11-21 11:28:00 +0100

    new script. plot mjj and fit. also with cut

--- scripts/plotScripts/plotDijetSpectrum.py
+++ scripts/plotScripts/plotDijetSpectrum.py
@@ -4,109 +4,226 @@ import glob
 import time
 import sys
 from matplotlib.ticker import AutoMinorLocator, LogLocator
+import matplotlib.patches as patches
+from scipy.optimize import curve_fit
+from scipy.stats import norm, crystalball
+from scipy.integrate import quad
+from utilsForPlot import loadData, loadDataOnlyMass, getXSectionBR
+import mplhep as hep
+hep.style.use("CMS")
 
-totalNanoEntries = 190153970
+afterCut, log = True, True
+if afterCut:
+    try:
+        signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/afterCutWithMoreFeatures/signalCut.npy"
+        realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData/afterCutWithMoreFeatures/realDataCut.npy"
+        signal = np.load(signalPath)[:,21]
+        realData = np.load(realDataPath)[:,21]
+    except:
+        print("Taken")
+        signalPath ="/t3home/gcelotto/ggHbb/outputs/signalCut.npy"
+        realDataPath="/t3home/gcelotto/ggHbb/outputs/realDataCut.npy"
+        signal = np.load(signalPath)[:,21]
+        realData = np.load(realDataPath)[:,21]
+else:
 
-signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/old"
-realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData"
+    # Loading files
+    signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/withMoreFeatures"
+    realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData/withMoreFeatures"
 
-signalFileNames = glob.glob(signalPath+"/*.npy")[:]
-bscore5FileNames = glob.glob(realDataPath+"/*bScoreBased5_*.npy")[:]
-bscore2FileNames = glob.glob(realDataPath+"/*bScoreBased2_*.npy")[:]
-bscore3FileNames = glob.glob(realDataPath+"/*bScoreBased3_*.npy")[:]
-bscore4FileNames = glob.glob(realDataPath+"/*bScoreBased4_*.npy")[:]
+    signal, realData = loadDataOnlyMass(signalPath=signalPath, realDataPath=realDataPath, nSignalFiles=-1, nRealDataFiles=40)
+
+# Correction factors and counters
+N_SignalMini = np.load("/t3home/gcelotto/bbar_analysis/output/N_mini.npy")
+N_DataNano = np.load("/t3home/gcelotto/ggHbb/outputs/N_BPH_Nano.npy")   
+correctionSignal = 1/N_SignalMini*getXSectionBR()*0.67*1000
+correctionData = N_DataNano/len(realData)*0.883
+#correctionData = 1017/300
+if afterCut:
+    print("Watchout")
+    correctionData = N_DataNano/len(realData)*0.883*0.0008896
+    correctionData = 1017/407
+    #
+# Counts numbers of data in 1A for signal and bkg
+totalSignalCounts = 0
+totalData = 0                   
+
+
+# Plot
+fig, (ax, ax2) = plt.subplots(nrows=2, sharex=True, gridspec_kw={'height_ratios': [4, 1]}, figsize=(6, 7))
+fig.subplots_adjust(hspace=0.0)
+fig.align_ylabels([ax,ax2])
+
+x1, x2, nbin = 0, 300, 101
+ax.set_xlim(x1, x2)
+bins = np.linspace(x1, x2, nbin)
+if log:
+    ax.set_xlim(x1, x2)
+    ax.set_yscale('log')
+
+# Prepare the points
+signalCounts = np.histogram (np.clip(signal, bins[0], bins[-1]), bins=bins)[0]
+realDataCounts = np.histogram(np.clip(realData, bins[0], bins[-1]), bins=bins)[0]
+# Get the error    
+realDataCountsErr= np.sqrt(realDataCounts)*correctionData
+signalCountsErr= np.sqrt(signalCounts)*correctionSignal #if log==True else np.sqrt(signalCounts)*correctionSignal*10**4
+#Normalize data
+signalCounts = signalCounts*correctionSignal #if log==True else signalCounts*correctionSignal*10**4
+realDataCounts = realDataCounts*correctionData
+#Keep track of all the expected entries at 1A lumi
+totalData += np.sum(realDataCounts)
+totalSignalCounts+=np.sum(signalCounts)
+# Plot data
+ax.hist(bins[:-1], bins=bins, weights=signalCounts, color='blue', histtype=u'step', label='MC ggHbb')#$\times10^4$
+ax.hist(bins[:-1], bins=bins, weights=realDataCounts, color='red', histtype=u'step', label='BParking Data')
+
+# Fit
+x_data_tot = (bins[:-1]+bins[1:])/2
+if afterCut is False:
+    x1_fr, x2_fr = 50, 165
+    gauss=False
+else:
+    x1_fr, x2_fr = 95, 170
+    gauss=True
+print("Fit region limited from %d to %d"%(x1_fr, x2_fr))
+fit_region = (x_data_tot>x1_fr) & (x_data_tot<x2_fr)
+y_fr = signalCounts[fit_region]
+x_fr = x_data_tot[fit_region]
+
+if gauss:
+    def gaussian_function(x, amplitude, mean, sigma):
+        return amplitude * norm.pdf(x, loc=mean, scale=sigma)   
+    initial_guess = [5*10**2, 125, 20]  # Initial guess for parameters
+    params, covariance = curve_fit(gaussian_function, x_fr, y_fr, p0=initial_guess)
+    amplitude_fit, mean_fit, sigma_fit,  = params
+    y_fit = gaussian_function(x_data_tot, amplitude_fit, mean_fit, sigma_fit)
+    result, abserr = quad(gaussian_function, x1_fr, x2_fr, args=(amplitude_fit, mean_fit, sigma_fit))[:2]
+
+else:
+    def crystal_ball_function(x, amplitude, mean, sigma, alpha, n):
+        return amplitude * crystalball.pdf(x, alpha, n, loc=mean, scale=sigma)
+    initial_guess = [3.56*10**3, 125, 20, 1, 10]  # Initial guess for parameters
+    params, covariance = curve_fit(crystal_ball_function, x_fr, y_fr, sigma = signalCountsErr[fit_region],p0=initial_guess)
+    amplitude_fit, mean_fit, sigma_fit, alpha_fit, n_fit = params
+    y_fit = crystal_ball_function(x_data_tot, amplitude_fit, mean_fit, sigma_fit, alpha_fit, n_fit)
+
+    num_samples=10
+    amplitude_samples = np.random.normal(amplitude_fit, np.sqrt(covariance[0, 0]), num_samples)
+    mean_samples = np.random.normal(mean_fit, np.sqrt(covariance[1, 1]), num_samples)
+    sigma_samples = np.random.normal(sigma_fit, np.sqrt(covariance[2, 2]), num_samples)
+    alpha_samples = np.random.normal(alpha_fit, np.sqrt(covariance[3, 3]), num_samples)
+    n_samples = np.random.normal(n_fit, np.sqrt(covariance[4, 4]), num_samples)
+    for i in range(num_samples):
+        print(amplitude_samples[i], mean_samples[i], sigma_samples[i], alpha_samples[i], n_fit)
+        result, _ = quad(crystal_ball_function, x1_fr, x2_fr, args=(amplitude_samples[i], mean_samples[i], sigma_samples[i], alpha_samples[i], n_fit))[:2]
+        print(result)
+
+    result, abserr= quad(crystal_ball_function, x1_fr, x2_fr, args=(amplitude_fit, mean_fit, sigma_fit, alpha_fit, n_fit))[:2]
+ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1])
+ax.plot(x_data_tot, y_fit, label='Fitted Curve', color='red')
+
+# Second plot
+ax2.hlines(y=0, xmin=x1, xmax=x2, color='black')
+residuals = (signalCounts-y_fit)/signalCountsErr
+ax2.errorbar(x_data_tot, residuals, yerr=1, color='black', linestyle=' ', marker='o')
+ax2.set_ylim(-5, 5)
+
+for par, var in zip(params, np.diag(covariance)):
+    print(par, " +- ", np.sqrt(var))
+ax.text(s=r"N$=%d\pm%d$"%(result/(bins[1]-bins[0]), abserr/(bins[1]-bins[0])), x=0.07, y=0.8,  ha='left', transform=ax.transAxes, fontsize=12)
+ax.text(s=r"$\mu=%.2f\pm%.2f$"%(mean_fit, np.sqrt(covariance[1, 1])),          x=0.07, y=0.75,  ha='left', transform=ax.transAxes, fontsize=12)
+ax.text(s=r"$\sigma=%.2f\pm%.2f$"%(sigma_fit, np.sqrt(covariance[2, 2])),      x=0.07, y=0.7,  ha='left', transform=ax.transAxes, fontsize=12)
+if gauss is False:
+    ax.text(s=r"$\alpha=%.2f\pm%.2f$"%(alpha_fit, np.sqrt(covariance[3, 3])),      x=0.07, y=0.65,  ha='left', transform=ax.transAxes, fontsize=12)
+    ax.text(s=r"n=$%.1f\pm%.1f$"%(n_fit, np.sqrt(covariance[4, 4])),      x=0.07, y=0.6,  ha='left', transform=ax.transAxes, fontsize=12)
+chi2 = np.sum(((y_fit[fit_region]-y_fr)/signalCountsErr[fit_region])**2)
+
+ndof = (len(y_fr)-len(params))
+ax.text(s=r"$\chi^2$/n$_\mathrm{dof}=%d/%d$"%(chi2, ndof),                      x=0.07, y=0.45,  ha='left', transform=ax.transAxes, fontsize=12)
+
+# Temporary check
+#fig2, ax2=plt.subplots(1, 1)
+#ax2.plot(x_data, y_data, label='Data', marker='o', color='black', linestyle=None)
+#ax2.plot(x_data, crystal_ball_function(x_data, 5*10**7, 125, 20, 1, 2))
+#ax2.legend()
+#fig2.savefig("/t3home/gcelotto/ggHbb/outputs/plots/temp.png", bbox_inches='tight')
+#End
 
 
 
-
-print("%d files for MC ggHbb" %len(signalFileNames))
-print("%d files for BPH 2018" %len(bscore5FileNames))
-print("%d files for leading Dijet" %len(bscore2FileNames))
-print("%d files for bscore3FileNames" %len(bscore3FileNames))
-print("%d files for bscore4FileNames" %len(bscore4FileNames))
-
-signal = np.load(signalFileNames[0])[:,19]
-for signalFileName in signalFileNames[1:]:
-    sys.stdout.write('\r')
-    sys.stdout.write("   %d/%d   "%(signalFileNames.index(signalFileName)+1, len(signalFileNames)))
-    sys.stdout.flush()
-    currentSignal = np.load(signalFileName)[:,19]
-    signal = np.concatenate((signal, currentSignal))
-print("Signal shape: ", signal.shape)
-
-bscore2 = np.load(bscore2FileNames[0])[:,17]
-for bscore2FileName in bscore2FileNames[1:]:
-    sys.stdout.write('\r')
-    sys.stdout.write("   %d/%d   "%(bscore2FileNames.index(bscore2FileName)+1, len(bscore2FileNames)))
-    sys.stdout.flush()
-    currentbscore2 = np.load(bscore2FileName)[:,17]
-    bscore2 = np.concatenate((bscore2, currentbscore2))
-print("bscore2 shape: ", bscore2.shape)
-
-bscore3 = np.load(bscore3FileNames[0])[:,17]
-for bscore3FileName in bscore3FileNames[1:]:
-    sys.stdout.write('\r')
-    sys.stdout.write("   %d/%d   "%(bscore3FileNames.index(bscore3FileName)+1, len(bscore3FileNames)))
-    sys.stdout.flush()
-    currentBscore3 = np.load(bscore3FileName)[:,17]
-    bscore3 = np.concatenate((bscore3, currentBscore3))
-print("bscore3 shape: ", bscore3.shape)
-
-bscore4 = np.load(bscore4FileNames[0])[:,17]
-for bscore4FileName in bscore4FileNames[1:]:
-    sys.stdout.write('\r')
-    sys.stdout.write("   %d/%d   "%(bscore4FileNames.index(bscore4FileName)+1, len(bscore4FileNames)))
-    sys.stdout.flush()
-    currentBscore4 = np.load(bscore4FileName)[:,17]
-    bscore4 = np.concatenate((bscore4, currentBscore4))
-print("bscore4 shape: ", bscore4.shape)
+# EXPECTED SIGNIFICANCE
+    #How many events between 95 and 165 GeV
+x1_sb, x2_sb  = mean_fit - 2*sigma_fit, mean_fit + 2*sigma_fit
+print(x1_sb, x2_sb)
+maskSignal = (signal[:]>x1_sb) & (signal[:]<x2_sb)
+maskData = (realData[:]>x1_sb) & (realData[:]<x2_sb)
     
-bscore5 = np.load(bscore5FileNames[0])[:,17]
-for bscore5FileName in bscore5FileNames[1:]:
-    sys.stdout.write('\r')
-    sys.stdout.write("   %d/%d   "%(bscore5FileNames.index(bscore5FileName)+1, len(bscore5FileNames)))
-    sys.stdout.flush()
-    #print(bscore5FileName)
-    currentbscore5 = np.load(bscore5FileName)[:,17]
-    bscore5 = np.concatenate((bscore5, currentbscore5))
-print("Real data shape: ", bscore5.shape)
+S = np.sum(maskSignal)*correctionSignal
+B = np.sum(maskData)*correctionData
+SErr = np.sqrt(np.sum(maskSignal))*correctionSignal
+BErr = np.sqrt(np.sum(maskData))*correctionData
+    
+Scommon_exponent = int("{:e}".format(SErr).split('e')[1])
+Bcommon_exponent = int("{:e}".format(BErr).split('e')[1])
+    
+Scommon_exponent = 0 if Scommon_exponent<0 else Scommon_exponent
+ax.text(s="0.67 fb$^{-1}$ (13 TeV)", x=1.00, y=1.02,  ha='right', transform=ax.transAxes, fontsize=16)
+if Scommon_exponent==0:
+    ax.text(s=r"S(2$\sigma$) = %d $\pm$ %d"%(S/(10**Scommon_exponent), SErr/(10**Scommon_exponent)),                                     x=0.93, y=0.45,  ha='right', transform=ax.transAxes, fontsize=12)
+else:
+    ax.text(s=r"S(2$\sigma$) = (%d $\pm$ %d) $\times e%d$"%(S/(10**Scommon_exponent), SErr/(10**Scommon_exponent), Scommon_exponent),      x=0.93, y=0.45,  ha='right', transform=ax.transAxes, fontsize=12)
+ax.text(s=r"B(2$\sigma$) = (%d $\pm$ %d) $\times 10^{%d}$"%(B/(10**Bcommon_exponent), BErr/(10**Bcommon_exponent), Bcommon_exponent),      x=0.93, y=0.4,  ha='right', transform=ax.transAxes, fontsize=12)
+ax.text(s="S/$\mathrm{\sqrt{B}}$ = %.1e"%(S/(np.sqrt(B))),                                                                      x=0.93, y=0.35,  ha='right', transform=ax.transAxes, fontsize=12)
+ax.text(s="S/B = %.1e"%(S/B),                                                                                                   x=0.93, y=0.3,  ha='right', transform=ax.transAxes, fontsize=12)
+# @ Full lumi
+ax.text(s="@Full BPH Lumi (41.6 fb$^{-1}$)",                                                                                      x=0.93, y=0.15,  ha='right', transform=ax.transAxes, fontsize=12)
+ax.text(s="S/$\mathrm{\sqrt{B}}$ = %.1e"%(S*np.sqrt(41.6/0.67)/(np.sqrt(B))),                                                     x=0.93, y=0.1,  ha='right', transform=ax.transAxes, fontsize=12)
 
 
-fig, ax = plt.subplots(1, 1)
-bins = np.linspace(1, 500, 500)
-bscore5Counts = np.histogram(bscore5[:], bins=bins)[0]
-signalCounts = np.histogram (signal[:], bins=bins)[0]
-bscore2Counts = np.histogram (bscore2[:], bins=bins)[0]
-bscore3Counts = np.histogram(bscore3[:], bins=bins)[0]
-bscore4Counts = np.histogram(bscore4[:], bins=bins)[0]
-realLumi = 0.67*np.sum(bscore2Counts)/totalNanoEntries
+
+ax2.set_xlabel("Dijet Mass [GeV]", fontsize=14)
 
 
-N_mini = np.load("/t3home/gcelotto/bbar_analysis/output/N_mini.npy")
 
 
-bscore5Counts = bscore5Counts*totalNanoEntries/np.sum(bscore5Counts)
-bscore2Counts = bscore2Counts*totalNanoEntries/np.sum(bscore2Counts)
-bscore3Counts = bscore3Counts*totalNanoEntries/np.sum(bscore3Counts)
-bscore4Counts = bscore4Counts*totalNanoEntries/np.sum(bscore4Counts)
-signalCounts = signalCounts/N_mini*30.52*0.67*1000
+for i in range(len(bins)-1):
+    if i ==0 :
+        rect = patches.Rectangle((bins[i], realDataCounts[i] - realDataCountsErr[i]),
+                bins[i+1]-bins[i], 2 *  realDataCountsErr[i],
+                linewidth=0, edgecolor='red', facecolor='none', hatch='///') #label='Uncertainty')
+    else:
+        rect = patches.Rectangle((bins[i], realDataCounts[i] - realDataCountsErr[i]),
+                bins[i+1]-bins[i], 2 *  realDataCountsErr[i],
+                linewidth=0, edgecolor='red', facecolor='none', hatch='///')
+    ax.add_patch(rect)
 
-ax.hist(bins[:-1], bins=bins, weights=signalCounts, color='blue', histtype=u'step', label='MC ggHbb')
-ax.hist(bins[:-1], bins=bins, weights=bscore2Counts, color='green', histtype=u'step', label='BParking Data n=2')
-ax.hist(bins[:-1], bins=bins, weights=bscore3Counts, color='pink', histtype=u'step', label='BParking Data n=3')
-ax.hist(bins[:-1], bins=bins, weights=bscore4Counts, color='orange', histtype=u'step', label='BParking Data n=4')
-ax.hist(bins[:-1], bins=bins, weights=bscore5Counts, color='red', histtype=u'step', label='BParking Data n=5')
+for i in range(len(bins)-1):
+    if i ==0 :
+        rect = patches.Rectangle((bins[i], signalCounts[i]*10**0 - signalCountsErr[i]*10**0),
+                bins[i+1]-bins[i], 2 *  signalCountsErr[i]*10**0,
+                linewidth=0, edgecolor='blue', facecolor='none', hatch='///') #label='Uncertainty')
+    else:
+        rect = patches.Rectangle((bins[i], signalCounts[i]*10**0 - signalCountsErr[i]*10**0),
+                bins[i+1]-bins[i], 2 *  signalCountsErr[i]*10**0,
+                linewidth=0, edgecolor='blue', facecolor='none', hatch='///')
+    ax.add_patch(rect)
+if not log:
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
 
-ax.set_xlabel("Dijet Mass [GeV]", fontsize=16)
+ax.vlines(x=[x1_fr, x2_fr], ymin=0, ymax=ax.get_ylim()[1], color='blue', alpha=0.2, label='Fit region')
+ax.vlines(x=[x1_sb, x2_sb], ymin=0, ymax=ax.get_ylim()[1], color='green', alpha=0.2, label=r'$\mu\pm2\sigma$')
+ax.legend(loc='upper right', fontsize=16)
+
 ax.set_ylabel("Events / %.1f GeV"%(bins[1]-bins[0]), fontsize=16)
-#ax.text(x=0.5, y = 0.5, s="Selected dijets:", fontsize=14, transform=fig.transFigure)
-#ax.text(x=0.5, y = 0.45, s="Max DeepFlavB sum", fontsize=14, transform=fig.transFigure)
-ax.text(s=r"%.5f fb$^{-1}$ (13 TeV)"%realLumi, x=1.00, y=1.02, ha='right', fontsize=12,  transform=ax.transAxes, **{'fontname':'Arial'})
-ax.set_yscale('log')
-#ax.grid()
-ax.tick_params(which='major', length=8)
-ax.xaxis.set_minor_locator(AutoMinorLocator())
-ax.yaxis.set_minor_locator(LogLocator(subs='all'))
-ax.tick_params(which='minor', length=4)
-ax.legend(loc='best')
-fig.savefig("/t3home/gcelotto/ggHbb/outputs/plots/dijetMass.pdf", bbox_inches='tight')
+ax2.set_ylabel(r"$\frac{\mathrm{Signal Events - Signal Fit}}{\mathrm{Signal Events Err}}$", fontsize=16)
+
+
+print(totalSignalCounts)
+print(totalData)
+if log:
+    outName = "/t3home/gcelotto/ggHbb/outputs/plots/dijetMassCut_log.png" if afterCut else "/t3home/gcelotto/ggHbb/outputs/plots/dijetMass_log.png"
+else:
+    outName = "/t3home/gcelotto/ggHbb/outputs/plots/dijetMassCut.png" if afterCut else "/t3home/gcelotto/ggHbb/outputs/plots/dijetMass.png"
+print("Saving in ", outName)
+fig.savefig(outName, bbox_inches='tight')
+
