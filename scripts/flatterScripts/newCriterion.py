b9468be99c60ff2c9247269ba8ba5e4ed1205d01
Giovanni Celotto <giovanni.celottog@gmail.com>
2023-11-23 11:35:26 +0100

    eta removed from matched. Saved results as pkl

--- scripts/flatterScripts/newCriterion.py
+++ scripts/flatterScripts/newCriterion.py
@@ -6,6 +6,7 @@ import ROOT
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import glob
 import os
+import pickle
 
 '''
 Choose a criterio to select the candidates jets that are most likely to come from the Higgs
@@ -27,13 +28,13 @@ def evaluateCriterion(maxJet, fileNames):
     
     goodChoice = 0
     wrongChoice = 0
-    totalChoice = 0
+    matched = 0
     nonMatched = 0
     outOfConsideredJets = 0
     totalEntriesVisited = 0
+    outOfEta=0
     print("\n***********************************************************************\n* Computing efficiency of criterion based on two  selected features \n***********************************************************************")
-    #path = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/0000/Hbb_QCDBackground_Run2_mc_2023Nov01_99.root"
-    #print("\nOpening path:", path, "...")
+    
     path = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/0000"
     
     for fileName in fileNames:
@@ -44,12 +45,10 @@ def evaluateCriterion(maxJet, fileNames):
         totalEntriesVisited += maxEntries
         print("\nFile %d/%d : %s\nEntries : %d"%(fileNames.index(fileName), len(fileNames), fileName[len(path)+1:], tree.num_entries))
     
-
         for ev in  range(maxEntries):
             
             if (ev%(int(maxEntries/100))==0):
                 sys.stdout.write('\r')
-                # the exact output you're looking for:
                 sys.stdout.write("%d%%"%(ev/maxEntries*100))
                 sys.stdout.flush()
                 pass
@@ -76,8 +75,8 @@ def evaluateCriterion(maxJet, fileNames):
 
             for i in range(nJet):
             # Find the jets from the signal
-                if (Jet_genJetIdx[i]>-1) & (abs(Jet_eta[i])<2.5):                                           # jet is matched to gen
-                    #if Jet_genJetIdx[i]<nGenJet:                                    # some events have jetGenIdx > nGenJet
+                if (Jet_genJetIdx[i]>-1):               # jet is matched to gen
+                    #if Jet_genJetIdx[i]<nGenJet:                               # some events have jetGenIdx > nGenJet
                     if abs(GenJet_partonFlavour[Jet_genJetIdx[i]])==5:          # jet matched to genjet from b
 
                         if GenJet_partonMotherPdgId[Jet_genJetIdx[i]]==25:      # jet parton mother is higgs (b comes from h)
@@ -91,13 +90,16 @@ def evaluateCriterion(maxJet, fileNames):
                 # take only events where there is the interested signal
                 nonMatched+=1
                 continue
-            totalChoice=totalChoice+1
+            else:
+                matched=matched+1
             assert idxJet1>-0.01
             assert idxJet2>-0.01
             
-
-            # idxJet1 and idxJet2 are the matched ones
-            
+            # if the reco jets are out of 2.5 no way that the choice will be correct
+            if ((abs(Jet_eta[idxJet1])>2.5)|(abs(Jet_eta[idxJet2])>2.5)):
+                outOfEta=outOfEta+1
+                continue
+                       
 
             jetsToCheck = np.min([maxJet, nJet])
             score=-999
@@ -151,14 +153,15 @@ def evaluateCriterion(maxJet, fileNames):
             
     print("\nTotal Entries visited                                      : %d  \t  %.2f" %(totalEntriesVisited, totalEntriesVisited/totalEntriesVisited*100))
     print("Non matched events or outOfAcceptance abs(eta)>2.5           : %d  \t  %.2f" %(nonMatched, nonMatched/totalEntriesVisited*100))
-    print("Events matched and within acceptance                         : %d  \t  %.2f" %(totalChoice, totalChoice/totalEntriesVisited*100))
+    print("Events matched and within acceptance                         : %d  \t  %.2f" %(matched, matched/totalEntriesVisited*100))
     print("Correct choice of jets                                       : %d  \t  %.2f" %(goodChoice, goodChoice/totalEntriesVisited*100))
     print("Wrong choice of jets                                         : %d  \t  %.2f" %(wrongChoice , wrongChoice/totalEntriesVisited*100))
-    print("No dijets found with a trigger muon within all the jets      : %d  \t  %.2f" %(outOfConsideredJets , outOfConsideredJets/totalEntriesVisited*100))
+    print("Wrong choice of jets                                         : %d  \t  %.2f" %(wrongChoice , wrongChoice/totalEntriesVisited*100))
+    print("Out of Eta                                                   : %d  \t  %.2f" %(outOfEta , outOfEta/totalEntriesVisited*100))
     
-    print("Consistency = nonMatched + correct + wrong + noDijetWithTrigger: %d  \t  %.1f" %(nonMatched+goodChoice+outOfConsideredJets+wrongChoice, (nonMatched+goodChoice+outOfConsideredJets+wrongChoice)/totalEntriesVisited))
+    print("Consistency = nonMatched + correct + wrong + noDijetWithTrigger: %d  \t  %.1f" %(nonMatched+goodChoice+outOfConsideredJets+wrongChoice+outOfEta, (nonMatched+goodChoice+outOfConsideredJets+wrongChoice)/totalEntriesVisited))
     
-    return 0
+    return nonMatched, matched, goodChoice, wrongChoice, outOfConsideredJets, outOfEta
 
 
 def main(nFiles, maxJet1, maxJet2):
@@ -175,14 +178,20 @@ def main(nFiles, maxJet1, maxJet2):
     fileNames = glob.glob(path+'/Hbb_QCDBackground_Run2_mc_2023Nov01*.root')
     fileNames = fileNames[:nFiles]
         
-
+    criterionSummary = {}
     for maxJet in range(maxJet1, maxJet2):
         print("nFiles                : ", nFiles)
         print("Max jet to check      : %d"%maxJet)
-        evaluateCriterion(maxJet, fileNames)
+        nonMatched, matched, goodChoice, wrongChoice, outOfConsideredJets, outOfEta = evaluateCriterion(maxJet, fileNames)
+        criterionSummary[maxJet] = [nonMatched, matched, goodChoice, wrongChoice, outOfConsideredJets, outOfEta]
+    with open("/t3home/gcelotto/ggHbb/outputs/dict_criterionEfficiency.pkl", 'wb') as file:
+        pickle.dump(criterionSummary, file)
+
+
+
 
 if __name__ == "__main__":
-    nFiles                   = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+    nFiles                   = int(sys.argv[1]) if len(sys.argv) > 1 else 20
     maxJet1                   = int(sys.argv[2]) if len(sys.argv) > 2 else 2
     maxJet2                   = int(sys.argv[3]) if len(sys.argv) > 3 else 8
     main(nFiles=nFiles, maxJet1=maxJet1, maxJet2=maxJet2)
