7a8c11a10b2fb6df69525e16787fc01c9822c7d8
Giovanni Celotto <giovanni.celottog@gmail.com>
2023-12-14 14:25:10 +0100

    final plot added

--- scripts/flatterScripts/newCriterion.py
+++ scripts/flatterScripts/newCriterion.py
@@ -7,6 +7,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import glob
 import os
 import pickle
+import sys
+sys.path.append('/t3home/gcelotto/ggHbb/scripts/plotScripts')
+from criterionEfficiencySummary import plotCriterionEfficiency
+from save_bscoreBased import jetsSelector
 
 '''
 Choose a criterio to select the candidates jets that are most likely to come from the Higgs
@@ -26,16 +30,21 @@ Args:
 # Now open the file and use the previous distribution
 def evaluateCriterion(maxJet, fileNames):
     
-    goodChoice = 0
-    wrongChoice = 0
-    matched = 0
-    nonMatched = 0
-    outOfConsideredJets = 0
-    totalEntriesVisited = 0
-    outOfEta=0
+    goodChoice,wrongChoice, matched, nonMatched, noPossiblePair, totalEntriesVisited, outOfEta = 0 ,  0,  0,  0,  0,  0, 0
+    # goodChoice       = events where criterion worked
+    # wrongChoice      = events criterion did not work
+    # mathced          = events in which Higgs daughters are matched
+    # nonMathced       = events in which Higgs daughters are not matched
+    # noPossiblePair   = events where no candidate dijet with trig muon inside, eta<2.5 and considering only the first N jets was found
+    # totalEntryVisited= all the events visited for checking some final computations
+    # outOfEta         = events that are matched but the reco jets are |eta| > 2.5 so btagging is not appropriate
+    wrongJetsMass = []
+    
+    differenceJetsQuarkPtEtaNonMathced = []
+    etaTracker = 2.5
     print("\n***********************************************************************\n* Computing efficiency of criterion based on two  selected features \n***********************************************************************")
     
-    path = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/0000"
+    path = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/ggH2023Dec06/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231206_105206/0000"
     
     for fileName in fileNames:
         f = uproot.open(fileName)
@@ -52,6 +61,11 @@ def evaluateCriterion(maxJet, fileNames):
                 sys.stdout.write("%d%%"%(ev/maxEntries*100))
                 sys.stdout.flush()
                 pass
+            nGenPart                    = branches["nGenPart"][ev]
+            GenPart_pdgId               = branches['GenPart_pdgId'][ev]
+            GenPart_genPartIdxMother    = branches["GenPart_genPartIdxMother"][ev]
+            GenPart_Pt                  = branches["GenPart_pt"][ev]
+            GenPart_Eta                 = branches["GenPart_eta"][ev]
             Jet_genJetIdx               = branches["Jet_genJetIdx"][ev]
             GenJet_partonFlavour        = branches["GenJet_partonFlavour"][ev]
             GenJet_partonMotherIdx      = branches["GenJet_partonMotherIdx"][ev]
@@ -64,7 +78,7 @@ def evaluateCriterion(maxJet, fileNames):
             Jet_muonIdx1                = branches["Jet_muonIdx1"][ev]
             Jet_muonIdx2                = branches["Jet_muonIdx2"][ev]
             Muon_isTriggering           = branches["Muon_isTriggering"][ev]
-            Jet_bRegNN2                 = branches["Jet_bRegNN2"][ev]
+            Jet_bReg2018                 = branches["Jet_bReg2018"][ev]
             Jet_btagDeepFlavB           = branches["Jet_btagDeepFlavB"][ev]
             Jet_nMuons                  = branches["Jet_nMuons"][ev]
             nGenJet                     = branches["nGenJet"][ev]
@@ -72,7 +86,7 @@ def evaluateCriterion(maxJet, fileNames):
             idxJet1, idxJet2 = -123, -124       # index of the first jet satisfying requirements
             numberOfGoodJets=0              # number of jets satisfying requirements per event
             #ht = 0
-
+            
             for i in range(nJet):
             # Find the jets from the signal
                 if (Jet_genJetIdx[i]>-1):               # jet is matched to gen
@@ -87,81 +101,80 @@ def evaluateCriterion(maxJet, fileNames):
                             elif GenJet_partonMotherIdx[Jet_genJetIdx[idxJet1]]==GenJet_partonMotherIdx[Jet_genJetIdx[i]]:  # second match. Also sisters
                                 idxJet2=i    
             if ((idxJet1==-123) | (idxJet2==-124)):
-                # take only events where there is the interested signal
+                # events of nonMatched
                 nonMatched+=1
+                mask = (GenPart_pdgId[GenPart_genPartIdxMother]==25) & (abs(GenPart_pdgId)==5)
+                if np.sum(mask)==2: # two true bquarks
+                    
+                    selected1, selected2, = jetsSelector(nJet, Jet_eta, Jet_muonIdx1,  Jet_muonIdx2, Muon_isTriggering, np.min([maxJet, nJet]), Jet_btagDeepFlavB)
+                    if (selected1!=999) & (selected2!=999):
+                        jet1 = ROOT.TLorentzVector(0., 0., 0., 0.)
+                        jet2 = ROOT.TLorentzVector(0., 0., 0., 0.)
+                        jet1.SetPtEtaPhiM(Jet_pt[selected1]*Jet_bReg2018[selected1],Jet_eta[selected1],Jet_phi[selected1],Jet_mass[selected1])
+                        jet2.SetPtEtaPhiM(Jet_pt[selected2]*Jet_bReg2018[selected2],Jet_eta[selected2],Jet_phi[selected2],Jet_mass[selected2])
+                        isQuark1Leading = True if GenPart_Pt[mask][0]>GenPart_Pt[mask][1] else False
+                        quark1Pt = GenPart_Pt[mask][0] if isQuark1Leading else GenPart_Pt[mask][1]
+                        quark1Eta = GenPart_Eta[mask][0] if isQuark1Leading else GenPart_Eta[mask][1]
+                        quark2Pt = GenPart_Pt[mask][1] if isQuark1Leading else GenPart_Pt[mask][0]
+                        quark2Eta = GenPart_Eta[mask][1] if isQuark1Leading else GenPart_Eta[mask][0]
+                        differenceJetsQuarkPtEtaNonMathced.append([(jet1.Pt() - quark1Pt)/quark1Pt, (jet1.Eta() - quark1Eta)/quark1Eta, (jet2.Pt() - quark2Pt)/quark2Pt, (jet2.Eta() - quark2Eta)/quark2Eta] )
                 continue
             else:
                 matched=matched+1
+                
             assert idxJet1>-0.01
             assert idxJet2>-0.01
             
+            jetsToCheck = np.min([maxJet, nJet])
+            
             # if the reco jets are out of 2.5 no way that the choice will be correct
-            if ((abs(Jet_eta[idxJet1])>2.5)|(abs(Jet_eta[idxJet2])>2.5)):
+            if ((abs(Jet_eta[idxJet1])>etaTracker)|(abs(Jet_eta[idxJet2])>etaTracker)):
                 outOfEta=outOfEta+1
+                selected1, selected2, = jetsSelector(nJet, Jet_eta, Jet_muonIdx1,  Jet_muonIdx2, Muon_isTriggering, jetsToCheck, Jet_btagDeepFlavB)
+                if (selected1!=999) & (selected2!=999):
+                    jet1 = ROOT.TLorentzVector(0., 0., 0., 0.)
+                    jet2 = ROOT.TLorentzVector(0., 0., 0., 0.)
+                    jet1.SetPtEtaPhiM(Jet_pt[selected1]*Jet_bReg2018[selected1],Jet_eta[selected1],Jet_phi[selected1],Jet_mass[selected1])
+                    jet2.SetPtEtaPhiM(Jet_pt[selected2]*Jet_bReg2018[selected2],Jet_eta[selected2],Jet_phi[selected2],Jet_mass[selected2])
+                    wrongJetsMass.append([(jet1+jet2).Pt(), (jet1+jet2).M()])
+
                 continue
                        
 
-            jetsToCheck = np.min([maxJet, nJet])
-            score=-999
-
-            # criterion 1:
-            selected1 = 999
-            selected2 = 999
-            jetsWithMuon = []
-            for i in range(nJet): # exclude the last jet because we are looking for pairs
-                if abs(Jet_eta[i])>2.5:
-                    continue
-                if (Jet_muonIdx1[i]>-1): #if there is a muon
-                    if (bool(Muon_isTriggering[Jet_muonIdx1[i]])):
-                        jetsWithMuon.append(i)
-                        continue
-                if (Jet_muonIdx2[i]>-1):
-                    if (bool(Muon_isTriggering[Jet_muonIdx2[i]])):
-                        jetsWithMuon.append(i)
-                        continue
-                
-            for i in jetsWithMuon:
-                for j in range(0, jetsToCheck):
-                    if i==j:
-                        continue
-                    if abs(Jet_eta[j])>2.5:
-                        continue
-
-                    
-                    jet1 = ROOT.TLorentzVector(0.,0.,0.,0.)
-                    jet2 = ROOT.TLorentzVector(0.,0.,0.,0.)
-                    jet1.SetPtEtaPhiM(Jet_pt[i]*Jet_bRegNN2[i], Jet_eta[i], Jet_phi[i], Jet_mass[i])
-                    jet2.SetPtEtaPhiM(Jet_pt[j]*Jet_bRegNN2[j], Jet_eta[j], Jet_phi[j], Jet_mass[j])
-                    # massDr criterion
-                    #deltaPhi = jet1.Phi()-jet2.Phi()
-                    #deltaPhi = deltaPhi - 2*np.pi*(deltaPhi > np.pi) + 2*np.pi*(deltaPhi< -np.pi)
-                    #tau = np.arctan(abs(deltaPhi)/abs(jet1.Eta() - jet2.Eta() + 0.0000001))
-
-                    currentScore = Jet_btagDeepFlavB[i] + Jet_btagDeepFlavB[j]
-                    if currentScore>score:
-                        score=currentScore
-                        selected1 = min(i, j)
-                        selected2 = max(i, j)
+            selected1, selected2, = jetsSelector(nJet, Jet_eta, Jet_muonIdx1,  Jet_muonIdx2, Muon_isTriggering, jetsToCheck, Jet_btagDeepFlavB)
 
             #print(ev, idxJet1, idxJet2, selected1, selected2)
-            if (idxJet1==selected1) & (idxJet2==selected2):
+            if (idxJet1==selected1) & (idxJet2==selected2): #choice we made is good
                 goodChoice=goodChoice+1
-            elif (selected1==999) & (selected2==999):
-                outOfConsideredJets+=1
+            elif (selected1==999) & (selected2==999):       #no choice made (event skipped)
+                noPossiblePair+=1
             else:
-                wrongChoice+=1
+                wrongChoice+=1                              # choice made not good
+                jet1 = ROOT.TLorentzVector(0., 0., 0., 0.)
+                jet2 = ROOT.TLorentzVector(0., 0., 0., 0.)
+                jet1.SetPtEtaPhiM(Jet_pt[selected1]*Jet_bReg2018[selected1],Jet_eta[selected1],Jet_phi[selected1],Jet_mass[selected1])
+                jet2.SetPtEtaPhiM(Jet_pt[selected2]*Jet_bReg2018[selected2],Jet_eta[selected2],Jet_phi[selected2],Jet_mass[selected2])
+                wrongJetsMass.append([(jet1+jet2).Pt(), (jet1+jet2).M()])
             
     print("\nTotal Entries visited                                      : %d  \t  %.2f" %(totalEntriesVisited, totalEntriesVisited/totalEntriesVisited*100))
-    print("Non matched events or outOfAcceptance abs(eta)>2.5           : %d  \t  %.2f" %(nonMatched, nonMatched/totalEntriesVisited*100))
-    print("Events matched and within acceptance                         : %d  \t  %.2f" %(matched, matched/totalEntriesVisited*100))
+    print("Non matched events                                           : %d  \t  %.2f" %(nonMatched, nonMatched/totalEntriesVisited*100))
+    print("Events matched                                               : %d  \t  %.2f" %(matched, matched/totalEntriesVisited*100))
     print("Correct choice of jets                                       : %d  \t  %.2f" %(goodChoice, goodChoice/totalEntriesVisited*100))
     print("Wrong choice of jets                                         : %d  \t  %.2f" %(wrongChoice , wrongChoice/totalEntriesVisited*100))
-    print("Wrong choice of jets                                         : %d  \t  %.2f" %(wrongChoice , wrongChoice/totalEntriesVisited*100))
+    print("No Dijet with trigger inside within 2.5. No choice made      : %d  \t  %.2f" %(noPossiblePair , noPossiblePair/totalEntriesVisited*100))
     print("Out of Eta                                                   : %d  \t  %.2f" %(outOfEta , outOfEta/totalEntriesVisited*100))
     
-    print("Consistency = nonMatched + correct + wrong + noDijetWithTrigger: %d  \t  %.1f" %(nonMatched+goodChoice+outOfConsideredJets+wrongChoice+outOfEta, (nonMatched+goodChoice+outOfConsideredJets+wrongChoice)/totalEntriesVisited))
+    print("Consistency = nonMatched + correct + wrong + noDijetWithTrigger + outOfEta: %d  \t  %.1f" %(nonMatched+goodChoice+noPossiblePair+wrongChoice+outOfEta, (nonMatched+goodChoice+noPossiblePair+wrongChoice)/totalEntriesVisited))
+    print(maxJet, maxJet==4)
+    if int(maxJet)==4:
+        print("Only for maxJet == 4 saving the wrong masses and non matched ones")
+        np.save("/t3home/gcelotto/ggHbb/outputs/wrongJetsMassCriterion.npy", wrongJetsMass)
+        np.save("/t3home/gcelotto/ggHbb/outputs/nonMatchedQuarksPt.npy", differenceJetsQuarkPtEtaNonMathced)
     
-    return nonMatched, matched, goodChoice, wrongChoice, outOfConsideredJets, outOfEta
+        print("Wrong jets mass saved length: ", len(wrongJetsMass))
+        print("Non matched jets saved length: ", len(differenceJetsQuarkPtEtaNonMathced))
+    
+    return nonMatched, matched, goodChoice, wrongChoice, noPossiblePair, outOfEta
 
 
 def main(nFiles, maxJet1, maxJet2):
@@ -174,18 +187,21 @@ def main(nFiles, maxJet1, maxJet2):
     '''
     
 
-    path = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/0000"
-    fileNames = glob.glob(path+'/Hbb_QCDBackground_Run2_mc_2023Nov01*.root')
+    path = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/ggH2023Dec06/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231206_105206/0000"
+    fileNames = glob.glob(path+'/ggH*.root')
     fileNames = fileNames[:nFiles]
         
     criterionSummary = {}
     for maxJet in range(maxJet1, maxJet2):
         print("nFiles                : ", nFiles)
         print("Max jet to check      : %d"%maxJet)
-        nonMatched, matched, goodChoice, wrongChoice, outOfConsideredJets, outOfEta = evaluateCriterion(maxJet, fileNames)
-        criterionSummary[maxJet] = [nonMatched, matched, goodChoice, wrongChoice, outOfConsideredJets, outOfEta]
+        nonMatched, matched, goodChoice, wrongChoice, noPossiblePair, outOfEta = evaluateCriterion(maxJet, fileNames)
+        criterionSummary[maxJet] = [nonMatched, matched, goodChoice, wrongChoice, noPossiblePair, outOfEta]
+    
     with open("/t3home/gcelotto/ggHbb/outputs/dict_criterionEfficiency.pkl", 'wb') as file:
         pickle.dump(criterionSummary, file)
+    plotCriterionEfficiency()
+    
 
 
 
