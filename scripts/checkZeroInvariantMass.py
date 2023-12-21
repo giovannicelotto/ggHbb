d8d84cab2ff7a40936eb1c503bae3da68baa129f
Giovanni Celotto <giovanni.celottog@gmail.com>
2023-12-21 10:25:06 +0100

    adapted too parquet

--- scripts/checkZeroInvariantMass.py
+++ scripts/checkZeroInvariantMass.py
@@ -2,20 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from plotScripts.utilsForPlot import loadData, loadDataOnlyFeatures, getXSectionBR
 import glob
-signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Hbb_QCDBackground2023Nov01/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231101_175738/flatData/withMoreFeatures"
-realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A2023Nov08/ParkingBPH1/crab_data_Run2018A_part1/231108_145003/flatData/withMoreFeatures"
+import pandas as pd
 
-signalFileNames = glob.glob(signalPath+"/*bScoreBased4_*.npy")
-realDataFileNames = glob.glob(realDataPath+"/*bScoreBased4_*.npy")
-
-for i in range(len(realDataFileNames)):
-    try:
-        realData = np.load(realDataFileNames[i])[:,[0, 1]]
-    except:
-        print("Not able to open %s"%realDataFileNames[i])
-    mask = realData[:,0]<5
-    l=np.sum(mask)
-    print("%d/%d   L=%d"%(i, len(realDataFileNames), l))
-    if l>1:
-        print(realDataFileNames[i])
-    
+def main():
+    signalPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/ggH2023Dec06/GluGluHToBB_M125_13TeV_powheg_pythia8/crab_GluGluHToBB/231206_105206/flatData"
+    realDataPath = "/pnfs/psi.ch/cms/trivcat/store/user/gcelotto/bb_ntuples/nanoaod_ggH/Data20181A_2023Nov30/ParkingBPH1/crab_data_Run2018A_part1/231130_120505/flatDataRoot"
+    corrupted=[]
+    signalFileNames = glob.glob(signalPath+"/*.parquet")
+    realDataFileNames = glob.glob(realDataPath+"/*.parquet")
+    for i in range(len(realDataFileNames)):
+        try:
+            realData = pd.read_parquet(realDataFileNames[i], columns=['dijet_mass'])
+        except:
+            print("Not able to open %s"%realDataFileNames[i])
+        mask = realData.iloc[:,0]<5
+        l=np.sum(mask)
+        print("%d/%d   L=%d"%(i+1, len(realDataFileNames), l))
+        if l>1:
+            print(realDataFileNames[i])
+            corrupted.append(realDataFileNames[i])
+    print("List of corrupted files:")
+    print(corrupted)
+if __name__=="__main__" :
+    main()
