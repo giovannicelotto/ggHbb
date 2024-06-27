8eb462a606567dc0ec8ac4111d8272e745c68531
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-06-27 16:54:55 +0200

    new arguments passed to tree flatter

--- flatter/job.sh
+++ flatter/job.sh
@@ -4,17 +4,20 @@
 #SBATCH --mem=2G                       # 2G for Data needed   
 #SBATCH --partition=short              # Specify your cluster partition
 #SBATCH --time=1:00:00  
-#SBATCH --output=/t3home/gcelotto/slurm/output/saveFlat.out  # Output file for stdout
-#SBATCH --error=/t3home/gcelotto/slurm/output/saveFlat.out    # Output file for stderr
+#SBATCH --output=/t3home/gcelotto/slurm/output/saveFlat_1.out  # Output file for stdout
+#SBATCH --error=/t3home/gcelotto/slurm/output/saveFlat_1.out    # Output file for stderr
 #SBATCH --dependency=singleton
 
-python /t3home/gcelotto/ggHbb/flatter/treeFlatter.py $@  # to pass all the arguments
 source_dir="/scratch"
-fileName="$1"
-process="$2"
-fileNumber="$3" 
-flatPath="$4"
+nanoFileName="$1"
+maxEntries="$2"
+maxJet="$3"
+isMC="$4"
+process="$5"
+fileNumber="$6" 
+flatPath="$7"
+python /t3home/gcelotto/ggHbb/flatter/treeFlatter.py $nanoFileName $maxEntries $maxJet $isMC $process # to pass all the arguments
 
-echo $flatPath
 
-xrdcp -f -N $source_dir/$process"_"$fileNumber.parquet root://t3dcachedb.psi.ch:1094//$4
+
+xrdcp -f -N $source_dir/$process"_"$fileNumber.parquet root://t3dcachedb.psi.ch:1094//$flatPath
