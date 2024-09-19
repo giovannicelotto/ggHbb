e8c4b143370a79c670df2a5d9d7f48ed357cb4bb
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-09-19 17:21:57 +0200

    add plots

--- PNN/PNNrun.py
+++ PNN/PNNrun.py
@@ -10,25 +10,41 @@ from helpers.getInfolderOutfolder import getInfolderOutfolder
 from helpers.scaleUnscale import scale, unscale
 from helpers.PNNClassifier import PNNClassifier
 from helpers.saveDataAndPredictions import save
+from helpers.doPlots import runPlots
+from helpers.flattenWeights import flattenWeights
+sys.path.append('/t3home/gcelotto/ggHbb/scripts/plotScripts')
+from plotFeatures import plotNormalizedFeatures
+
 
 inFolder, outFolder = getInfolderOutfolder()
-featuresForTraining, columnsToRead = getFeatures(inFolder)
+featuresForTraining, columnsToRead = getFeatures(outFolder)
 hp = getParams()
-nReal = 5
+nReal = 10
 nMC = -1
 
         
 data = loadData(nReal, nMC, outFolder, columnsToRead, featuresForTraining, hp)
 Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest = data
+rWtrain, rWtest = flattenWeights(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, outName=outFolder+ "/performance/massReweighted.png")
 
-    
-Xtrain = scale(Xtrain, scalerName= inFolder + "/myScaler.pkl" ,fit=True)
-Xtest  = scale(Xtest, scalerName= inFolder + "/myScaler.pkl" ,fit=False)
-             
-YPredTrain, YPredTest = PNNClassifier(Xtrain=Xtrain, Xtest=Xtest, Ytrain=Ytrain, Ytest=Ytest, Wtrain=Wtrain, Wtest=Wtest,
-                                        hp=hp, inFolder=inFolder, outFolder=outFolder)
+plotNormalizedFeatures(data=[Xtrain[Ytrain==0], Xtrain[Ytrain==1], Xtest[Ytest==0], Xtest[Ytest==1]],
+                       outFile=outFolder+"/performance/features.png", legendLabels=['Data Train', 'Higgs Train', 'Data Test', 'Higgs Test'],
+                       colors=['blue', 'red', 'blue', 'red'], histtypes=[u'step', u'step', 'bar', 'bar'],
+                       alphas=[1, 1, 0.4, 0.4], figsize=(10,30), autobins=False,
+                       weights=[Wtrain[Ytrain==0], Wtrain[Ytrain==1], Wtest[Ytest==0], Wtest[Ytest==1]], error=True)
 
-Xtrain = unscale(Xtrain,    scalerName= inFolder + "/myScaler.pkl")
-Xtest = unscale(Xtest,      scalerName =  inFolder + "/myScaler.pkl")
+Xtrain = scale(Xtrain, scalerName= outFolder + "/model/myScaler.pkl" ,fit=True)
+Xtest  = scale(Xtest, scalerName= outFolder + "/model/myScaler.pkl" ,fit=False)
+
+
+YPredTrain, YPredTest, model, featuresForTraining = PNNClassifier(Xtrain=Xtrain, Xtest=Xtest, Ytrain=Ytrain, Ytest=Ytest, Wtrain=Wtrain, Wtest=Wtest,
+                                                    rWtrain=rWtrain, rWtest=rWtest, featuresForTraining=featuresForTraining,
+                                                    hp=hp, inFolder=inFolder, outFolder=outFolder)
+
+Xtrain = unscale(Xtrain,    scalerName= outFolder + "/model/myScaler.pkl")
+Xtest = unscale(Xtest,      scalerName =  outFolder + "/model/myScaler.pkl")
 
 save(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, YPredTrain, YPredTest, inFolder)
+
+# Plots 
+runPlots(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, YPredTrain, YPredTest, featuresForTraining, model, inFolder, outFolder)
