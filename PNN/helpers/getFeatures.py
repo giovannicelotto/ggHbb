8479e833c409e7dd88b6569d7e87021a25a933c2
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-09-21 09:55:56 +0200

    add masshypo as argument

--- PNN/helpers/getFeatures.py
+++ PNN/helpers/getFeatures.py
@@ -1,6 +1,6 @@
 import numpy as np
-def getFeatures(outFolder):
-    featuresForTraining=[
+def getFeatures(outFolder=None, massHypo=False):
+   featuresForTraining=[
        #'jet1_pt',
         'jet1_eta',
         'jet1_phi',
@@ -41,10 +41,9 @@ def getFeatures(outFolder):
        'nSV',
        'muon_pfRelIso03_all',
        'muon_tkIsoId',
-       'muon_tightId',
-       ]
+       'muon_tightId']
     
-    columnsToRead = [   
+   columnsToRead = [   
     'jet1_pt', 'jet1_eta', 'jet1_phi', 'jet1_mass', 'jet1_nMuons', 'jet1_nTightMuons',
     'jet1_nElectrons', 'jet1_btagDeepFlavB', 'jet1_area', 'jet1_qgl',
     'jet2_pt', 'jet2_eta', 'jet2_phi', 'jet2_mass', 'jet2_nMuons','jet2_nTightMuons',
@@ -58,6 +57,54 @@ def getFeatures(outFolder):
     'dijet_cs', 'normalized_dijet_pt',
 
     'sf']
-    if outFolder is not None:
+   if outFolder is not None:
       np.save(outFolder+"/model/featuresForTraining.npy", featuresForTraining)
-    return featuresForTraining, columnsToRead
+   if massHypo==True:
+      featuresForTraining = featuresForTraining + ['massHypo']
+   return featuresForTraining, columnsToRead
+
+
+'''featuresForTraining=[
+       'jet1_pt',
+        'jet1_eta',
+        'jet1_phi',
+       'jet1_mass',
+         'jet1_nMuons',
+        'jet1_nTightMuons',
+       'jet1_nElectrons',
+        'jet1_btagDeepFlavB', #'jet1_area',
+        'jet1_qgl',
+       'jet2_pt',
+        'jet2_eta',
+        'jet2_phi',
+        'jet2_mass',
+       'jet2_nMuons',
+        'jet2_nTightMuons',
+       'jet2_nElectrons',
+        'jet2_btagDeepFlavB', #'jet2_area',
+        'jet2_qgl',
+        'jet3_pt',
+        'jet3_eta', 'jet3_phi',
+        'jet3_mass',
+        'normalized_dijet_pt',
+       'dijet_pt',
+    'dijet_eta', 'dijet_phi',
+       'dijet_mass',
+       'dijet_dR',
+       'dijet_dEta', 'dijet_dPhi',
+       'dijet_twist',# 'nJets',
+       'nJets_20GeV',
+       'ht',
+    'muon_pt',
+       'muon_eta',
+       'muon_dxySig',  
+       'muon_dzSig',
+       'muon_IP3d',
+       'muon_sIP3d',
+       'dijet_cs',
+       'nSV',
+       'muon_pfRelIso03_all',
+       'muon_tkIsoId',
+       'muon_tightId',
+       ]    
+       '''
