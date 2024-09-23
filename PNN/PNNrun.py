78eac3db9324da92a46cb0c02bab3c3e80aa2806
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-09-23 11:49:28 +0200

    comment added

--- PNN/PNNrun.py
+++ PNN/PNNrun.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import mplhep as hep
 hep.style.use("CMS")
 import sys
+sys.path.append('/t3home/gcelotto/ggHbb/scripts/plotScripts')
+from plotFeatures import plotNormalizedFeatures
 sys.path.append("/t3home/gcelotto/ggHbb/PNN/helpers")
 
 from helpers.getFeatures import getFeatures
@@ -14,30 +16,34 @@ from helpers.saveDataAndPredictions import save
 from helpers.doPlots import runPlots
 from helpers.flattenWeights import flattenWeights
 
-sys.path.append('/t3home/gcelotto/ggHbb/scripts/plotScripts')
-from plotFeatures import plotNormalizedFeatures
 
-
+# Define folder of input and output. Create the folders if not existing
 inFolder, outFolder = getInfolderOutfolder()
+# Define features to read and to train the pNN (+parameter massHypo) and save the features for training in outfolder
 featuresForTraining, columnsToRead = getFeatures(outFolder)
+# define the parameters for the nn
 hp = getParams()
-nReal = 10
-nMC = -1
+# number of files from real data and mc
+nReal, nMC = 10, -1
 
-        
+# load data for the samples and preprocess the data(pT cut)
+# fill the massHypo column
+# cut the data to have same length in all the samples
+# reweight each sample to have total weight 1, shuffle and split in train and test
 data = loadData(nReal, nMC, outFolder, columnsToRead, featuresForTraining, hp)
 Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest = data
+# Higgs and Data have flat distribution in m_jj
 rWtrain, rWtest = flattenWeights(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, inFolder, outName=outFolder+ "/performance/massReweighted.png")
 
-plotNormalizedFeatures(data=[Xtrain[Ytrain==0], Xtrain[Ytrain==1], Xtest[Ytest==0], Xtest[Ytest==1]],
-                       outFile=outFolder+"/performance/features.png", legendLabels=['Data Train', 'Higgs Train', 'Data Test', 'Higgs Test'],
-                       colors=['blue', 'red', 'blue', 'red'], histtypes=[u'step', u'step', 'bar', 'bar'],
-                       alphas=[1, 1, 0.4, 0.4], figsize=(10,30), autobins=False,
-                       weights=[Wtrain[Ytrain==0], Wtrain[Ytrain==1], Wtest[Ytest==0], Wtest[Ytest==1]], error=True)
+#plotNormalizedFeatures(data=[Xtrain[Ytrain==0], Xtrain[Ytrain==1], Xtest[Ytest==0], Xtest[Ytest==1]],
+#                       outFile=outFolder+"/performance/features.png", legendLabels=['Data Train', 'Higgs Train', 'Data Test', 'Higgs Test'],
+#                       colors=['blue', 'red', 'blue', 'red'], histtypes=[u'step', u'step', 'bar', 'bar'],
+#                       alphas=[1, 1, 0.4, 0.4], figsize=(10,30), autobins=False,
+#                       weights=[Wtrain[Ytrain==0], Wtrain[Ytrain==1], Wtest[Ytest==0], Wtest[Ytest==1]], error=True)
 featuresForTraining = featuresForTraining + ['massHypo']
 Xtrain = scale(Xtrain,featuresForTraining,  scalerName= outFolder + "/model/myScaler.pkl" ,fit=True)
 print(Xtrain.jet1_eta.mean()), Xtrain.jet1_eta.std()
-sys.exit()
+
 Xtest  = scale(Xtest, featuresForTraining, scalerName= outFolder + "/model/myScaler.pkl" ,fit=False)
 
 
