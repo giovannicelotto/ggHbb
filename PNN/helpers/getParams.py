89ddb557099651f725987629ec4de89f53f1462a
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-09-23 15:31:45 +0200

    new param batch size

--- PNN/helpers/getParams.py
+++ PNN/helpers/getParams.py
@@ -6,6 +6,7 @@ def getParams():
     'test_split'        : 0.2,
     'learning_rate'     : 5*1e-5,
     'nNodes'            : [64, 32, 16],
+    'batch_size'        : 2048,
         }
     hp['nDense']=len(hp['nNodes'])
     return hp
