6fc8f186277c1065fffc3fdbcc834b9b3f8fcd91
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-09-23 15:30:41 +0200

    comments addded for clarification

--- PNN/PNNrun.py
+++ PNN/PNNrun.py
@@ -19,10 +19,13 @@ from helpers.flattenWeights import flattenWeights
 
 # Define folder of input and output. Create the folders if not existing
 inFolder, outFolder = getInfolderOutfolder()
+
 # Define features to read and to train the pNN (+parameter massHypo) and save the features for training in outfolder
 featuresForTraining, columnsToRead = getFeatures(outFolder)
+
 # define the parameters for the nn
 hp = getParams()
+
 # number of files from real data and mc
 nReal, nMC = 10, -1
 
@@ -32,28 +35,31 @@ nReal, nMC = 10, -1
 # reweight each sample to have total weight 1, shuffle and split in train and test
 data = loadData(nReal, nMC, outFolder, columnsToRead, featuresForTraining, hp)
 Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest = data
+
 # Higgs and Data have flat distribution in m_jj
 rWtrain, rWtest = flattenWeights(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, inFolder, outName=outFolder+ "/performance/massReweighted.png")
 
-#plotNormalizedFeatures(data=[Xtrain[Ytrain==0], Xtrain[Ytrain==1], Xtest[Ytest==0], Xtest[Ytest==1]],
-#                       outFile=outFolder+"/performance/features.png", legendLabels=['Data Train', 'Higgs Train', 'Data Test', 'Higgs Test'],
-#                       colors=['blue', 'red', 'blue', 'red'], histtypes=[u'step', u'step', 'bar', 'bar'],
-#                       alphas=[1, 1, 0.4, 0.4], figsize=(10,30), autobins=False,
-#                       weights=[Wtrain[Ytrain==0], Wtrain[Ytrain==1], Wtest[Ytest==0], Wtest[Ytest==1]], error=True)
+plotNormalizedFeatures(data=[Xtrain[Ytrain==0], Xtrain[Ytrain==1], Xtest[Ytest==0], Xtest[Ytest==1]],
+                       outFile=outFolder+"/performance/features.png", legendLabels=['Data Train', 'Higgs Train', 'Data Test', 'Higgs Test'],
+                       colors=['blue', 'red', 'blue', 'red'], histtypes=[u'step', u'step', 'bar', 'bar'],
+                       alphas=[1, 1, 0.4, 0.4], figsize=(10,30), autobins=False,
+                       weights=[Wtrain[Ytrain==0], Wtrain[Ytrain==1], Wtest[Ytest==0], Wtest[Ytest==1]], error=True)
 featuresForTraining = featuresForTraining + ['massHypo']
-Xtrain = scale(Xtrain,featuresForTraining,  scalerName= outFolder + "/model/myScaler.pkl" ,fit=True)
-print(Xtrain.jet1_eta.mean()), Xtrain.jet1_eta.std()
 
+# scale with standard scalers and apply log to any pt and mass distributions
+Xtrain = scale(Xtrain,featuresForTraining,  scalerName= outFolder + "/model/myScaler.pkl" ,fit=True)
 Xtest  = scale(Xtest, featuresForTraining, scalerName= outFolder + "/model/myScaler.pkl" ,fit=False)
 
-
+# define the model and fit and make predictions
 YPredTrain, YPredTest, model, featuresForTraining = PNNClassifier(Xtrain=Xtrain, Xtest=Xtest, Ytrain=Ytrain, Ytest=Ytest, Wtrain=Wtrain, Wtest=Wtest,
                                                     rWtrain=rWtrain, rWtest=rWtest, featuresForTraining=featuresForTraining,
                                                     hp=hp, inFolder=inFolder, outFolder=outFolder)
 
+# unscale in order to make plots 
 Xtrain = unscale(Xtrain, featuresForTraining=featuresForTraining, scalerName= outFolder + "/model/myScaler.pkl")
 Xtest = unscale(Xtest, featuresForTraining=featuresForTraining,   scalerName =  outFolder + "/model/myScaler.pkl")
 
+# save all the data in the inFolder
 save(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, YPredTrain, YPredTest, inFolder)
 
 # Plots 
