e07c43c9497aea13b4f17aba5e1c3898b286e30d
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-09-24 07:41:03 +0200

    untracked

--- PNN/PNNplotOnly.py
+++ PNN/PNNplotOnly.py
@@ -9,6 +9,8 @@ from helpers.getInfolderOutfolder import getInfolderOutfolder
 from helpers.doPlots import runPlots
 from helpers.loadSaved import loadSaved
 from tensorflow.keras.models import load_model
+sys.path.append('/t3home/gcelotto/ggHbb/scripts/plotScripts')
+from plotFeatures import plotNormalizedFeatures
 
 inFolder, outFolder = getInfolderOutfolder()
 modelName = "myModel.h5"
@@ -17,5 +19,11 @@ featuresForTraining = featuresForTraining + ["massHypo"]
 hp = getParams()
 
 Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, YPredTrain, YPredTest = loadSaved(inFolder)
+print(Xtest.dijet_mass)
+#plotNormalizedFeatures(data=[Xtrain[Ytrain==0], Xtrain[Ytrain==1], Xtest[Ytest==0], Xtest[Ytest==1]],
+#                       outFile=outFolder+"/performance/features.png", legendLabels=['Data Train', 'Higgs Train', 'Data Test', 'Higgs Test'],
+#                       colors=['blue', 'red', 'blue', 'red'], histtypes=[u'step', u'step', 'bar', 'bar'],
+#                       alphas=[1, 1, 0.4, 0.4], figsize=(10,30), autobins=False,
+#                       weights=[Wtrain[Ytrain==0], Wtrain[Ytrain==1], Wtest[Ytest==0], Wtest[Ytest==1]], error=True)
 model = load_model(outFolder +"/model/"+modelName)
 runPlots(Xtrain, Xtest, Ytrain, Ytest, Wtrain, Wtest, YPredTrain, YPredTest, featuresForTraining, model, inFolder, outFolder)
