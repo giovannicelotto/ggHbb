689485b7900cdb20d8f7c0f73d971a5c7bea0e7b
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-10-22 12:51:35 +0200

    fixed scaler with weights

--- PNN/helpers/scaleUnscale.py
+++ PNN/helpers/scaleUnscale.py
@@ -5,19 +5,30 @@ import numpy as np
 
 
 def scale(data, featuresForTraining, scalerName, fit=False, weights=None):
+    '''
+    apply log to features with pt and mass in name
     
+    
+    
+    
+    
+    '''
     for colName in featuresForTraining:
         if ("_pt" in colName) | ("_mass" in colName) | (colName=="ht"):
+            if "normalized" in colName:
+                #print("normalized found in ", colName)
+                continue
             print("feature: %s min: %.1f max: %.1f"%(colName, data[colName].min(), data[colName].max()))
             data[colName] = np.log(1+data[colName])
             #print("log done for %s"%colName)
 # fitting the scaler
     if fit:
-        scaler  = preprocessing.StandardScaler().fit(data[[col for col in featuresForTraining if col!='sf']])
         if weights is not None:
-            scaled_array = scaler.transform(data[[col for col in featuresForTraining if col!='sf']], sample_weights=weights)
+            scaler  = preprocessing.StandardScaler().fit(data[[col for col in featuresForTraining if col!='sf']], sample_weights=weights)
         else:
-            scaled_array = scaler.transform(data[[col for col in featuresForTraining if col!='sf']])
+            scaler  = preprocessing.StandardScaler().fit(data[[col for col in featuresForTraining if col!='sf']])
+
+        scaled_array = scaler.transform(data[[col for col in featuresForTraining if col!='sf']])
         scalers = {
             'type'  : 'standard',
             'scaler': scaler,
@@ -29,10 +40,7 @@ def scale(data, featuresForTraining, scalerName, fit=False, weights=None):
         with open(scalerName, 'rb') as file:
             scalers = pickle.load(file)
             scaler = scalers['scaler']
-            if weights is not None:
-                scaled_array = scaler.transform(data[[col for col in featuresForTraining if col != 'sf']], sample_weight=weights)
-            else:
-                scaled_array = scaler.transform(data[[col for col in featuresForTraining if col != 'sf']])
+            scaled_array = scaler.transform(data[[col for col in featuresForTraining if col != 'sf']])
             
     dataScaled = pd.DataFrame(scaled_array, columns=[col for col in featuresForTraining if col!='sf'], index=data.index)
     for col in data.columns:
@@ -49,11 +57,14 @@ def unscale(data, featuresForTraining, scalerName):
         scaler = scalers['scaler']
         scaled_array = scaler.inverse_transform(data[[col for col in featuresForTraining if col!='sf']])
         dataUnscaled = pd.DataFrame(scaled_array, columns=[col for col in featuresForTraining if col!='sf'], index=data.index)
-        dataUnscaled['sf'] = data['sf']
+        #dataUnscaled['sf'] = data['sf']
 
     
     for colName in featuresForTraining:
         if ("_pt" in colName) | ("_mass" in colName) | (colName=="ht"):
             dataUnscaled[colName] = np.exp(dataUnscaled[colName]) - 1
     
+    for feature in data.columns:
+        if feature not in featuresForTraining:
+            dataUnscaled[feature] = data[feature]
     return dataUnscaled
